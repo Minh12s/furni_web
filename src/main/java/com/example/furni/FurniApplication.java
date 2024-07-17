1b028340f00693563399689823e039017ed950a1
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-07-18 00:15:43 +0700

    update

---
+++ src/main/java/com/example/furni/FurniApplication.java
@@ -0,0 +1,13 @@
+package com.example.furni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/furni/FurniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.furni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
