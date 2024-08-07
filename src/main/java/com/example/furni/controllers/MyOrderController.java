64874e2d35f8ecd55f283cf637101a61e259bef1
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-08-07 13:41:59 +0700

    update myorder

--- src/main/java/com/example/furni/controllers/MyOrderController.java
+++ src/main/java/com/example/furni/controllers/MyOrderController.java
@@ -24,5 +24,49 @@ public class MyOrderController {
     public String EditProfile(){
         return "MyOrder/EditProfile";
     }
+    @GetMapping("/OrderCancel")
+    public String OrderCancel(){
+        return "MyOrder/OrderCancel";
+    }
+    @GetMapping("/OrderPending")
+    public String OrderPending(){
+        return "MyOrder/OrderPending";
+    }
+    @GetMapping("/OrderConfirmed")
+    public String OrderConfirmed(){
+        return "MyOrder/OrderConfirmed";
+    }
+    @GetMapping("/OrderShipping")
+    public String OrderShipping(){
+        return "MyOrder/OrderShipping";
+    }
+    @GetMapping("/OrderShipped")
+    public String OrderShipped(){
+        return "MyOrder/OrderShipped";
+    }
+    @GetMapping("/OrderComplete")
+    public String OrderComplete(){
+        return "MyOrder/OrderComplete";
+    }
+    @GetMapping("/OrderReturn")
+    public String OrderReturn(){
+        return "MyOrder/OrderReturn";
+    }
+    @GetMapping("/OrderDetail")
+    public String OrderDetail(){
+        return "MyOrder/OrderDetail";
+    }
+    @GetMapping("/Review")
+    public String Review(){
+        return "MyOrder/Review";
+    }
+    @GetMapping("/RequestRefund")
+    public String RequestRefund(){
+        return "MyOrder/RequestRefund";
+    }
+    @GetMapping("/ReasonCancel")
+    public String ReasonCancel(){
+        return "MyOrder/ReasonCancel";
+    }
 
 }
